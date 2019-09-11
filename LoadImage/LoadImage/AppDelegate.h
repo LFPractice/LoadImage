d3a7dc484c62ec9f977fcd0d822a8f88531d3d87
WangLinfang <1261876974@qq.com>
2019-09-11 10:38:01 +0800

    工程初始化

---
+++ LoadImage/LoadImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadImage
+//
+//  Created by 王林芳 on 2019/9/11.
+//  Copyright © 2019 王林芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LoadImage/LoadImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadImage
+//
+//  Created by 王林芳 on 2019/9/11.
+//  Copyright © 2019 王林芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
